0b7cdfaa7b0e3e9b9ff5be189a88b2c52af38025
Mateus Arruda Navarro Albuquerque <mateusnavarro@2ndmarket.com.br>
2023-03-10 09:37:53 -0300

    added base64 library

--- contracts/ERC721Template.sol
+++ contracts/ERC721Template.sol
@@ -6,10 +6,11 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract ERC721Template is ERC721URIStorage, Ownable {
-    using Counters for uint256;
-
+    using Counters for Counters.Counter;
+    using Strings for uint256;
+    
     string private _ipfsCollection;
-    uint256 private _tokenCounter;
+    Counters.Counter private _tokenCounter;
     string private _imageFileExtension;
 
     constructor(
@@ -20,6 +21,18 @@ contract ERC721Template is ERC721URIStorage, Ownable {
     ) ERC721(name, symbol) {
         _ipfsCollection = ipfsCollection;
         _imageFileExtension = imageFileExtension;
+    }
+
+    function mintNFT() public {
+        string memory newTokenUri;
+        uint256 tokenId = _tokenCounter.current();
+        
+        newTokenUri = string(abi.encodePacked(getIpfsCollection(),tokenId.toString(),getImageFileExtension()));
+        
+        _safeMint(msg.sender, tokenId);
+        _setTokenURI(tokenId, newTokenUri);
+        
+        _tokenCounter.increment();
     }
 
     function getIpfsCollection() public view returns (string memory) {
@@ -35,10 +48,70 @@ contract ERC721Template is ERC721URIStorage, Ownable {
     function getImageFileExtension() public view returns (string memory) {
         return _imageFileExtension;
     }
-    function setImageFileExtension(string memory newFileExtension) external onlyOwner() {
+
+    function setImageFileExtension(
+        string memory newFileExtension
+    ) external onlyOwner {
         _imageFileExtension = newFileExtension;
     }
-    function getTokenCounter() public view returns (uint256) {
-        return _tokenCounter;
+
+
+}
+
+library Base64 {
+    string internal constant TABLE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+
+    function encode(bytes memory data) internal pure returns (string memory) {
+        if (data.length == 0) return '';
+        
+        // load the table into memory
+        string memory table = TABLE;
+
+        // multiply by 4/3 rounded up
+        uint256 encodedLen = 4 * ((data.length + 2) / 3);
+
+        // add some extra buffer at the end required for the writing
+        string memory result = new string(encodedLen + 32);
+
+        assembly {
+            // set the actual output length
+            mstore(result, encodedLen)
+            
+            // prepare the lookup table
+            let tablePtr := add(table, 1)
+            
+            // input ptr
+            let dataPtr := data
+            let endPtr := add(dataPtr, mload(data))
+            
+            // result ptr, jump over length
+            let resultPtr := add(result, 32)
+            
+            // run over the input, 3 bytes at a time
+            for {} lt(dataPtr, endPtr) {}
+            {
+               dataPtr := add(dataPtr, 3)
+               
+               // read 3 bytes
+               let input := mload(dataPtr)
+               
+               // write 4 characters
+               mstore(resultPtr, shl(248, mload(add(tablePtr, and(shr(18, input), 0x3F)))))
+               resultPtr := add(resultPtr, 1)
+               mstore(resultPtr, shl(248, mload(add(tablePtr, and(shr(12, input), 0x3F)))))
+               resultPtr := add(resultPtr, 1)
+               mstore(resultPtr, shl(248, mload(add(tablePtr, and(shr( 6, input), 0x3F)))))
+               resultPtr := add(resultPtr, 1)
+               mstore(resultPtr, shl(248, mload(add(tablePtr, and(        input,  0x3F)))))
+               resultPtr := add(resultPtr, 1)
+            }
+            
+            // padding with '='
+            switch mod(mload(data), 3)
+            case 1 { mstore(sub(resultPtr, 2), shl(240, 0x3d3d)) }
+            case 2 { mstore(sub(resultPtr, 1), shl(248, 0x3d)) }
+        }
+        
+        return result;
     }
 }
