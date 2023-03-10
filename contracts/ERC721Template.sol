29794240761906ea24b172fd9747139a9532beca
Mateus Arruda Navarro Albuquerque <mateusnavarro@2ndmarket.com.br>
2023-03-10 08:44:23 -0300

    comeco de um templateERC721

---
+++ contracts/ERC721Template.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ERC721Template is ERC721URIStorage,Ownable {
+    
+    using Counters for uint256;
+
+    
+    string private _ipfsCollection;
+    
+    string private _imageFileExtension;
+
+
+    constructor(
+        string memory name,
+        string memory symbol,
+        string memory ipfsCollection,
+        string memory imageFileExtension
+    ) ERC721(name, symbol) {
+        _ipfsCollection = ipfsCollection;
+        _imageFileExtension = imageFileExtension;
+    }
+
+    function getIpfsCollection() public view returns(string memory){
+        return _ipfsCollection;
+
+    }
+
+    function setIpfsCollection() external  onlyOwner(){}
+
+
+
+
+
+}
