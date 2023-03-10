ea33469d9e0ccb77f8e6a4d69c9d238902a302c6
Mateus Arruda Navarro Albuquerque <mateusnavarro@2ndmarket.com.br>
2023-03-10 08:52:01 -0300

    cria os getters e os setters

--- contracts/ERC721Template.sol
+++ contracts/ERC721Template.sol
@@ -5,15 +5,12 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ERC721Template is ERC721URIStorage,Ownable {
-    
+contract ERC721Template is ERC721URIStorage, Ownable {
     using Counters for uint256;
 
-    
     string private _ipfsCollection;
     uint256 private _tokenCounter;
     string private _imageFileExtension;
-
 
     constructor(
         string memory name,
@@ -25,19 +22,23 @@ contract ERC721Template is ERC721URIStorage,Ownable {
         _imageFileExtension = imageFileExtension;
     }
 
-    function getIpfsCollection() public view returns(string memory){
+    function getIpfsCollection() public view returns (string memory) {
         return _ipfsCollection;
-
     }
 
-    function setIpfsCollection(string memory newCollectionUrl) external  onlyOwner(){
+    function setIpfsCollection(
+        string memory newCollectionUrl
+    ) external onlyOwner {
         _ipfsCollection = newCollectionUrl;
     }
 
-    function getTokenCounter() public view returns(uint256){
+    function getImageFileExtension() public view returns (string memory) {
+        return _imageFileExtension;
+    }
+    function setImageFileExtension(string memory newFileExtension) external onlyOwner() {
+        _imageFileExtension = newFileExtension;
+    }
+    function getTokenCounter() public view returns (uint256) {
         return _tokenCounter;
     }
-
-
-
 }
